486ef9a5dd759356d01ae29b70efe57c9354e3cb
Boda Ferenc Bence <bence@Boda-MacBook-Air.local>
2024-11-19 14:42:50 +0100

    Finished, bugs fixed

--- projectcannibal.py
+++ projectcannibal.py
@@ -22,7 +22,7 @@ def getstates (state, c):
                     new_k_jobb = k_jobb + k_in_csonak
                     new_v_jobb = v_jobb + v_in_csonak
                     if validstate(new_k_bal, new_v_bal, new_k_jobb, new_v_jobb, c):
-                        allapot_valtozas.append((new_k_bal, new_v_bal, new_k_jobb, new_v_jobb, "right"))
+                        allapot_valtozas.append((new_k_bal, new_v_bal, new_k_jobb, new_v_jobb, "jobb"))
     else:  # A csónak a jobb parton van
         for k_in_csonak in range(3):  # kannibálok számának meghatározása: 0, 1 vagy 2
             for v_in_csonak in range(3 - k_in_csonak):  # 0, 1, vagy 2 vegetáriánus
@@ -40,20 +40,20 @@ def kidolgozas (c=1):
     # A feladat megoldása, ha az erőfölény (C) = 1
     kezdo_allapot = (3, 3, 0, 0, "bal") 
     cel_allapot = (0, 0, 3, 3, "jobb") # Ez lenne a cél: 3 kannibál és 3 vegetáriánus a jobb parton 
-    visited = set()
+    vizsgalt_utak = set() # Ez a tömb azért kell, hogy ne hajtsuk végre ugyanazt az utat kétszer
     queue = [(kezdo_allapot, [])]  # Lista mint sor: (jelenlegi állapot, útvonal)
 
     while queue:
         jelen_allapot, utvonal = queue.pop(0)  # Az első elemet vesszük ki (FIFO)
-        if jelen_allapot in visited:
+        if jelen_allapot in vizsgalt_utak:
             continue
-        visited.add(jelen_allapot)
+        vizsgalt_utak.add(jelen_allapot)
         
         if jelen_allapot == cel_allapot:
             return utvonal + [jelen_allapot]
         
-        for next_state in getstates(jelen_allapot, c):
-            queue.append((next_state, utvonal + [jelen_allapot]))
+        for kov_allapot in getstates(jelen_allapot, c):
+            queue.append((kov_allapot, utvonal + [jelen_allapot]))
     
     return None
 
